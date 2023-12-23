56f9496745cf45fa5dd33be1d6ce7ba82a5e9b55
Mateusz Dobek <mat.dobek@gmail.com>
2023-12-23 23:44:50 +0100

    Modularize setup file

--- setup.sh
+++ setup.sh
@@ -3,114 +3,181 @@
 dotfiles_dir=$HOME/main/friday/dotfiles
 
 #================
-# system
+# Wayland
 #================
 
-ln -sf $dotfiles_dir/system/.xkb_layout.xkb ~/
-ln -sf $dotfiles_dir/system/.xinitrc ~/
+function hyprland () {
+  mkdir -p ~/.config/hypr
+  ln -sf $dotfiles_dir/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+}
+
+function waybar () {
+  rm -rf ~/.config/waybar
+  mkdir -p ~/.config/waybar
+
+  ln -sf $dotfiles_dir/waybar/* ~/.config/waybar/
+}
 
 #================
-# xmonad
+# X
 #================
 
-mkdir -p ~/.xmonad
+function xserver () {
+  ln -sf $dotfiles_dir/system/.xkb_layout.xkb ~/
+  ln -sf $dotfiles_dir/system/.xinitrc ~/
+}
 
-ln -sf $dotfiles_dir/xmonad/xmonad.hs ~/.xmonad/
-ln -sf $dotfiles_dir/xmonad/background.jpg ~/.xmonad/
+
+function xmonad () {
+  mkdir -p ~/.xmonad
+
+  ln -sf $dotfiles_dir/xmonad/xmonad.hs ~/.xmonad/
+  ln -sf $dotfiles_dir/xmonad/background.jpg ~/.xmonad/
+}
 
 #================
 # eww
 #================
 
-mkdir -p ~/.config/eww
-mkdir -p ~/.config/eww/modules
+function eww () {
+  mkdir -p ~/.config/eww
+  mkdir -p ~/.config/eww/modules
 
-ln -sf $dotfiles_dir/eww/eww.scss ~/.config/eww/
-ln -sf $dotfiles_dir/eww/eww.yuck ~/.config/eww/
+  ln -sf $dotfiles_dir/eww/eww.scss ~/.config/eww/
+  ln -sf $dotfiles_dir/eww/eww.yuck ~/.config/eww/
 
-ln -sf $dotfiles_dir/eww/modules/notifications.sh ~/.config/eww/modules/
-ln -sf $dotfiles_dir/eww/modules/workspaces.sh ~/.config/eww/modules/
+  ln -sf $dotfiles_dir/eww/modules/notifications.sh ~/.config/eww/modules/
+  ln -sf $dotfiles_dir/eww/modules/workspaces.sh ~/.config/eww/modules/
+}
 
 #================
-# custom scripts
+# custom
 #================
 
-mkdir -p ~/.local/bin
+function scripts () {
+  mkdir -p ~/.local/bin
 
-ln -sf $dotfiles_dir/scripts/* ~/.local/bin/
+  ln -sf $dotfiles_dir/scripts/* ~/.local/bin/
+}
+
+function wallpapers () {
+  mkdir -p ~/.config/wallpapers
+
+  ln -sf $dotfiles_dir/wallpapers/* ~/.config/wallpapers/
+}
+
 
 #================
 # alacritty
 #================
 
-mkdir -p ~/.config/alacritty
+function alacritty () {
+  mkdir -p ~/.config/alacritty
 
-ln -sf $dotfiles_dir/alacritty/alacritty.yml ~/.config/alacritty/
+  ln -sf $dotfiles_dir/alacritty/alacritty.yml ~/.config/alacritty/
+}
 
 #================
 # kitty
 #================
 
-mkdir -p ~/.config/kitty
+function kitty() {
+  mkdir -p ~/.config/kitty
 
-ln -sf $dotfiles_dir/kitty/kitty.conf ~/.config/kitty/
+  ln -sf $dotfiles_dir/kitty/kitty.conf ~/.config/kitty/
+}
 
 #================
 # bash
 #================
 
-ln -sf $dotfiles_dir/bash/.bashrc ~/
-ln -sf $dotfiles_dir/bash/.bash_aliases ~/
-ln -sf $dotfiles_dir/bash/.bash_secrets.sample ~/
-ln -sf $dotfiles_dir/bash/.bash_profile ~/
-ln -sf $dotfiles_dir/bash/.inputrc ~/
+function bash() {
+  ln -sf $dotfiles_dir/bash/.bashrc ~/
+  ln -sf $dotfiles_dir/bash/.bash_aliases ~/
+  ln -sf $dotfiles_dir/bash/.bash_secrets.sample ~/
+  ln -sf $dotfiles_dir/bash/.bash_profile ~/
+  ln -sf $dotfiles_dir/bash/.inputrc ~/
+}
 
 #================
 # git
 #================
 
-ln -sf $dotfiles_dir/git/.gitconfig ~/
-ln -sf $dotfiles_dir/git/.gitignore ~/
-
-#================
-# ctags
-#================
-
-ln -sf $dotfiles_dir/ctags/.ctags ~/
+function git() {
+  ln -sf $dotfiles_dir/git/.gitconfig ~/
+  ln -sf $dotfiles_dir/git/.gitignore ~/
+}
 
 #================
 # tmux
 #================
 
-rm -rf ~/.tmux_sessions
-mkdir -p ~/.tmux_sessions
+function tmux() {
+  rm -rf ~/.tmux_sessions
+  mkdir -p ~/.tmux_sessions
 
-ln -sf $dotfiles_dir/tmux/.tmux.conf ~/
-ln -sf $dotfiles_dir/tmux/sessions/* ~/.tmux_sessions/
+  ln -sf $dotfiles_dir/tmux/.tmux.conf ~/
+  ln -sf $dotfiles_dir/tmux/sessions/* ~/.tmux_sessions/
+}
 
 #================
 # nvim
 #================
 
-rm -rf ~/.config/nvim
-mkdir -p ~/.config/nvim
+function nvim() {
+  rm -rf ~/.config/nvim
+  mkdir -p ~/.config/nvim
 
-ln -sf $dotfiles_dir/nvim/init.lua ~/.config/nvim/
+  ln -sf $dotfiles_dir/nvim/init.lua ~/.config/nvim/
 
-ln -sf $dotfiles_dir/nvim/after ~/.config/nvim/
-ln -sf $dotfiles_dir/nvim/lua ~/.config/nvim/
+  ln -sf $dotfiles_dir/nvim/after ~/.config/nvim/
+  ln -sf $dotfiles_dir/nvim/lua ~/.config/nvim/
+}
 
 #================
 # ruby
 #================
 
-ln -sf $dotfiles_dir/ruby/.gemrc ~/
+function ruby() {
+  ln -sf $dotfiles_dir/ruby/.gemrc ~/
+}
 
 
 #================
 # gpg
 #================
 
-mkdir -p ~/.gnupg
+function gpg() {
+  mkdir -p ~/.gnupg
 
-ln -sf $dotfiles_dir/gnupg/gpg-agent.conf ~/.gnupg
+  ln -sf $dotfiles_dir/gnupg/gpg-agent.conf ~/.gnupg
+}
+
+
+
+#================
+#================
+
+
+
+hyprland
+waybar
+
+xserver
+xmonad
+eww
+
+alacritty
+kitty
+
+wallpapers
+scripts
+
+bash
+git
+gpg
+
+tmux
+nvim
+
+ruby
