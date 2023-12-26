771f49040949e64f2903c4a1fad57651c101bc35
Mateusz Dobek <mat.dobek@gmail.com>
2023-12-26 23:27:19 +0100

    listen on wayland socket for workspace changes

--- eww/modules/workspaces_wayland.sh
+++ eww/modules/workspaces_wayland.sh
@@ -1,19 +1,81 @@
-#!/bin/sh
+#!/bin/bash
+
+function generete_eww {
+  local active_workspace=$(($1))
+  local workspaces=("${@:2}")
+
+  rslt="(box :class \"workspace\" :spacing 5"
+
+  for i in "${!workspaces[@]}"; do
+    local current=${workspaces[$i]}
+    local next=${workspaces[$(($i + 1))]}
+
+    if (( ${active_workspace} == ${current} )); then
+      rslt+=" (workspace :current true :active true)"
+    else
+      rslt+=" (workspace :current false :active true)"
+    fi
+
+    if [ "$next" != "" ]; then
+      for (( j=${workspaces[i]} + 1; j < ${workspaces[i+1]}; j++ )); do
+        rslt+=" (workspace :current false :active false)"
+      done
+    fi
+  done
+
+  rslt+=")"
+
+  echo $rslt
+}
+
+function put {
+  local x="$1"
+  local xs=("${@:2}")
+
+  xs+=(${x})
+
+  for x in "${xs[@]}"; do
+    echo "${x}"
+  done | uniq -u | sort -g | xargs echo
+}
+
+function drop {
+  local e="$1"
+  local xs=("${@:2}")
+
+  for x in "${xs[@]}"; do
+    if [ "$x" != "$e" ]; then
+      echo "${x}"
+    fi
+  done | xargs echo
+}
 
 workspaces=($(hyprctl workspaces -j | jq '.[] | select(.id > 0) | .id' | sort -g))
 active_workspace=$(hyprctl activeworkspace -j | jq .id)
 
-rslt="(box :class \"workspace\" :spacing 5 "
+generete_eww "${active_workspace}" "${workspaces[@]}"
 
-for workspace in "${workspaces[@]}"; do
-  is_active=false
+socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
+  IFS=">>"; arr=($line); unset IFS
 
-  if (($active_workspace == workspace)); then
-    is_active=true
-  fi
+  command="${arr[0]}"
+  workspace_no="${arr[2]}"
 
-  rslt+="(workspace :active { ${is_active} }) "
+  case ${command} in
+    "workspace")
+      active_workspace="${workspace_no}"
+      generete_eww "${active_workspace}" "${workspaces[@]}"
+      ;;
+    "createworkspace")
+      workspaces=($(put "${workspace_no}" "${workspaces[@]}"))
+      generete_eww "${active_workspace}" "${workspaces[@]}"
+      ;;
+    "destroyworkspace")
+      workspaces=($(drop "${workspace_no}" "${workspaces[@]}"))
+      generete_eww "${active_workspace}" "${workspaces[@]}"
+      ;;
+    *)
+      :
+      ;;
+  esac
 done
-
-rslt+=")"
-echo $rslt
